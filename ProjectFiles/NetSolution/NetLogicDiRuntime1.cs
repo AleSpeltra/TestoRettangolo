8a793dbfa308b53f565a5abec5c6d2d0f791eb74
alespe <a.speltra@asem.it>
2022-09-14 12:32:46 +0200

    netlogic

---
+++ ProjectFiles/NetSolution/NetLogicDiRuntime1.cs
@@ -0,0 +1,24 @@
+#region Using directives
+using System;
+using UAManagedCore;
+using OpcUa = UAManagedCore.OpcUa;
+using QPlatform.HMIProject;
+using QPlatform.NativeUI;
+using QPlatform.UI;
+using QPlatform.NetLogic;
+using QPlatform.CoreBase;
+using QPlatform.Core;
+#endregion
+
+public class NetLogicDiRuntime1 : BaseNetLogic
+{
+    public override void Start()
+    {
+        // Insert code to be executed when the user-defined logic is started
+    }
+
+    public override void Stop()
+    {
+        // Insert code to be executed when the user-defined logic is stopped
+    }
+}
